4b9ad0c40de48816c63c46c591a2f0956206f858
Francesco Cantarella <fcantarella@trovimap.com>
2019-07-17 11:02:25 +0200

    migrated to angular 8.1.1

--- projects/ng-bootstrap-form-validation/src/lib/Components/form-group/form-group.component.ts
+++ projects/ng-bootstrap-form-validation/src/lib/Components/form-group/form-group.component.ts
@@ -49,7 +49,10 @@ export class FormGroupComponent implements OnInit, AfterContentInit {
     );
   }
 
-  @ContentChild(MessagesComponent)
+  @ContentChild(
+    MessagesComponent,
+    /* TODO: add static flag */ { static: false }
+  )
   public messagesBlock: MessagesComponent;
 
   private errorMessages: ErrorMessage[];
